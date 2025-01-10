23433df8aa78a0613cb50afa4df5bda28fb1e190
Lucas Jimenez Sepulveda <125527408+LucasJS28@users.noreply.github.com>
2025-01-10 01:03:08 -0300

    Arreglando desplazamientos

--- scripts.js
+++ scripts.js
@@ -1,10 +1,1 @@
-// Activar o desactivar el menú en pantallas pequeñas
-const mobileMenu = document.getElementById("mobile-menu");
-const navLinks = document.getElementById("nav-links");
-
-mobileMenu.addEventListener("click", () => {
-    navLinks.classList.toggle("active");
-});
-
-
 
